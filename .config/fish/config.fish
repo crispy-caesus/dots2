fb288319f66a647aeb4a008a4edab3bf7d809b81
crispy-caesus <114518720+crispy-caesus@users.noreply.github.com>
2024-12-09 23:18:57 +0100

    fish: set nvim editor env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ if status is-interactive
     # tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_spacing=Compact --icons='Few icons' --transient=No
 
     set -gx PATH $PATH ~/.local/bin/
+    set -Ux EDITOR nvim
 
     alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
