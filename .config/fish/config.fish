ddd2b27ecd53999962d45f40193fcf9121721c09
crispy-caesus <114518720+crispy-caesus@users.noreply.github.com>
2025-03-02 23:45:52 +0100

    fish: secrets

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,7 @@ if status is-interactive
     alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
     source ~/.config/fish/functions/bindings.fish
+    source ~/.config/fish/secrets.fish
 end
 
 set fish_greeting ""
