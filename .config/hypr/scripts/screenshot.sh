ebd074ec5488e81190e4558cea592073999fbac9
crispy-caesus <114518720+crispy-caesus@users.noreply.github.com>
2025-04-03 17:16:28 +0200

    hyprland: add freezed editable screenshot

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -2,13 +2,16 @@
 
 temp_screenshot="/tmp/screenshot.png"
 
-while getopts "ase" flag; do
+while getopts "asemf" flag; do
   case "${flag}" in
     a) grimblast copy area;;
     s) grimblast copy screen;;
     e) grimblast copysave area $temp_screenshot && swappy -f $temp_screenshot;;
     m) grimblast copy output;;
+    f) grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot;;
   esac
 done
+
+echo $temp_screenshot
 
 rm "$temp_screenshot"
