3bd2dfad6acc8ccca8f32ce7ae426324b89f6af6
crispy-caesus <crispy@crispy-caesus.eu>
2025-05-26 11:23:17 +0200

    hyprland: scripts: screenshot - remove grimblast - handle freezing with wayfreeze - stop saving to /tmp

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-temp_screenshot="/tmp/screenshot.png"
-
 OPTIONS=a,s,e,m,f
-LONGOPTS=area,screen,edit,monitor,freeze
+LONGOPTS=area,screen,edit,monitor,freeze,freezeedit
 
 PARSED=$(getopt --options="$OPTIONS" --longoptions="$LONGOPTS" -- "$@")
 eval set -- "$PARSED"
 
 while true; do
     case "$1" in
-        -a|--area) grimblast copy area
+        -a|--area) grim -g "$(slurp)" - | wl-copy
             shift;;
-        -s|--screen) grimblast copy screen
+        -s|--screen) grim - | wl-copy
             shift;;
-        -e|--edit) grimblast copysave area $temp_screenshot && swappy -f $temp_screenshot
+        -e|--edit) exec 3< <(grim -g "$(slurp)" -)
+            swappy -f - <&3
             shift;;
-        -m|--monitor) grimblast copy output
+        -m|--monitor) grim -o "$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')" - | wl-copy
             shift;;
-        -f|--freeze) grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot
+        -f|--freeze) wayfreeze --hide-cursor --after-freeze-cmd 'grim -g "$(slurp)" - | wl-copy; killall wayfreeze'
+            shift;;
+        --freezeedit) wayfreeze & PID=$!; sleep .1; kill $PID && exec 3< <(grim -g "$(slurp)" -) && swappy -f - <&3;
             shift;;
         --) shift; break;;
     esac
 done
-
-echo $temp_screenshot
-
-rm "$temp_screenshot"
