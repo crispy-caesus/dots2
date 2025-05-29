25043fb0030073f670d12b1f19879a717e1324be
crispy-caesus <crispy@crispy-caesus.eu>
2025-05-29 13:47:50 +0200

    hyprland: screenrecord switch to wl-screenrec probably works

--- .config/hypr/scripts/screenrecord.sh
+++ .config/hypr/scripts/screenrecord.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # Stop wf-recorder if it's already running
-if pgrep -x "wf-recorder" > /dev/null; then
-  pkill -INT -x wf-recorder
-  notify-send -h string:wf-recorder:record -t 1000 "Finished Recording"
-  exit 0
+if pgrep -x "wl-screenrec" > /dev/null; then
+    pkill -INT -x wl-screenrec
+    notify-send -h string:wl-screenrec:record -t 1000 "Finished Recording"
+    exit 0
 fi
 
 flags=""
-fileextention="mp4"
+fileextention="mkv"
 output="-o DP-2"  # Default output
 
 # Parse options using getopts
 while getopts "ae:lo:" flag; do  # ':' for options with arguments
-  case "${flag}" in
-    a) flags="${flags} -a=$(pactl get-default-sink).monitor";;            # Append the -a flag
-    e) fileextention=${OPTARG};;            # Set the file format from -f option
-    l) flags="${flags} -b 5000k -r 30";;        # sets lower bitrate and framerate
-    o) output="-o ${OPTARG}";;           # Set the output device from -o option
-    *) echo "Invalid option"; exit 1;;   # Handle invalid options
-  esac
+    case "${flag}" in
+        a) flags="${flags} --audio --audio-device="$(pactl get-default-sink).monitor"";;            # Append the -a flag
+        e) fileextention=${OPTARG};;            # Set the file format from -f option
+        l) flags="${flags} -b 1MB --ffmpeg-encoder h264_vaapi --ffmpeg-encoder-options \"crf=28,preset=veryfast\""        # sets lower bitrate and framerate
+            fileextention="mp4";;
+        o) output="-o ${OPTARG}";;           # Set the output device from -o option
+        *) echo "Invalid option"; exit 1;;   # Handle invalid options
+    esac
 done
 
 # Add the output flag to the flags
@@ -31,7 +32,7 @@ echo "File format: ${fileformat}"
 
 
 # Debugging: Print the full command that will be executed
-echo "Running command: wf-recorder -f ~/Videos/recording.${fileformat} -y ${flags}"
+echo "Running command: wl-screenrec -f ~/Videos/recording.${fileformat} ${flags}"
 
 # Start wf-recorder with the specified format, flags, and output
-wf-recorder -f ~/Videos/recording.${fileextention} -y ${flags}
+wl-screenrec -f ~/Videos/recording.${fileextention} ${flags}
