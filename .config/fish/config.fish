5d4c464ccc694ba0565717d25b21cd487c4f6486
crispy-caesus <crispy@crispy-caesus.eu>
2025-06-16 20:41:51 +0200

    hyprland, fish: add uwsm, remove some startups

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
+
     # tmux a
 
     # fastfetch
     #starship init fish | source
-    
+
     # tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_spacing=Compact --icons='Few icons' --transient=No
+
+
+
+    if uwsm check may-start && uwsm select
+        exec uwsm start default
+    end
+
 
     set -gx PATH $PATH ~/.local/bin/ ~/go/bin
     set -Ux EDITOR nvim
