e01e7df21908784b345098f9c9fbc3ff8cf83bf8
crispy-caesus <crispy@crispy-caesus.eu>
2025-07-08 17:53:57 +0200

    fish: cleanup, hydro, theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,17 +3,9 @@ if status is-interactive
 
     # tmux a
 
-    # fastfetch
-    #starship init fish | source
-
-    # tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_spacing=Compact --icons='Few icons' --transient=No
-
-
-
     if uwsm check may-start && uwsm select
         exec uwsm start default
     end
-
 
     set -gx PATH $PATH ~/.local/bin/ ~/go/bin
     set -Ux EDITOR nvim
@@ -26,6 +18,20 @@ if status is-interactive
 
     set -U tide_right_promt_items
     set CHROOT $HOME/chroot
+    # ===================== theme ================ #
+    fish_config theme choose "Rosé Pine"
+
+    set -U hydro_color_pwd $fish_color_pine
+    set -U hydro_color_git $fish_color_foam
+    set -U hydro_color_start $fish_color_iris
+    set -U hydro_color_error $fish_color_love
+    set -U hydro_color_prompt $fish_color_pine
+    set -U hydro_color_duration $fish_color_iris
+    set -U fish_prompt_pwd_dir_length 0
+    set -g fish_color_search_match
+    set -g fish_pager_color_background
+    set -g fish_pager_color_selected_background
+
 end
 
 set fish_greeting ""
